e20ae53e0175997b42fd397f9050a7fc84a57a5b
lfd <2281409362@qq.com>
2026-02-15 23:50:42 +0800

    encoding

---
+++ lldb/util/encoding.h
@@ -0,0 +1,109 @@
+#pragma once
+
+#include <unistd.h>
+
+#include <cstdint>
+#include <string>
+
+#include "slice.h"
+
+namespace lldb {
+
+enum class Endian {
+  LITTLE = __ORDER_LITTLE_ENDIAN__,
+  BIG = __ORDER_BIG_ENDIAN__,
+  NATIVE = __BYTE_ORDER__,//当前机器字节序
+};
+
+// Determine the size segment used by this machine
+
+constexpr inline bool IsLittleEndian() {
+  return Endian::NATIVE == Endian::LITTLE;
+}
+
+constexpr inline bool IsBigEndian() { return Endian::NATIVE == Endian::BIG; }
+//字节翻转
+constexpr inline uint32_t BitSwap(uint32_t x) { return __builtin_bswap32(x); }
+
+constexpr inline uint64_t BitSwap(uint64_t x) { return __builtin_bswap64(x); }
+
+// 把整数 value 写入 buf（固定字节数）
+template <typename T>
+constexpr char *EncodeFixed(char *buf, T value) {
+  if constexpr (IsLittleEndian()) {
+    value = BitSwap(value);
+  }
+  __builtin_memcpy(buf, &value, sizeof(value));
+  return buf + sizeof(value);
+}
+
+inline char *EncodeFixed32(char *buf, uint32_t value) {
+  return EncodeFixed<uint32_t>(buf, value);
+}
+inline char *EncodeFixed64(char *buf, uint64_t value) {
+  return EncodeFixed<uint64_t>(buf, value);
+}
+
+// 定长整数解码
+template <typename T>
+constexpr T DecodeFixed(const char *ptr) {
+  T value = 0;
+
+  __builtin_memcpy(&value, ptr, sizeof(value));
+
+  return IsLittleEndian() ? BitSwap(value) : value;
+}
+
+inline uint32_t DecodeFixed32(const char *ptr) {
+  return DecodeFixed<uint32_t>(ptr);
+}
+inline uint64_t DecodeFixed64(const char *ptr) {
+  return DecodeFixed<uint64_t>(ptr);
+}
+
+template <typename T>
+void PutFixed(std::string *dst, T value) {
+  char buf[sizeof(value)];
+  EncodeFixed(buf, value);
+  dst->append(buf, sizeof(buf));
+}
+//将一个定长整数编码后追加到 dst 字符串中
+inline void PutFixed32(std::string *dst, uint32_t value) {
+  PutFixed<uint32_t>(dst, value);
+}
+inline void PutFixed64(std::string *dst, uint64_t value) {
+  PutFixed<uint64_t>(dst, value);
+}
+// 从输入数据中读取一个定长整数
+template <typename T>
+bool GetFixed(Slice *input, T *value) {
+  if (input.size() < sizeof(T)) return false;
+  *value = DecodeFixed<T>(input.data());
+  input->remove_prefixe(sizeof(T));
+  return true;
+}
+
+inline bool GetFixed32(Slice *input, uint32_t *value) {
+  return GetFixed<uint32_t>(input, value);
+}
+inline bool GetFixed64(Slice *input, uint64_t *value) {
+  return GetFixed<uint64_t>(input, value);
+}// namespace Tskydb
+
+char *EncodeVarint32(char *dst, uint32_t v);
+char *EncodeVarint64(char *dst, uint64_t v);
+
+void PutVarint32(std::string *dst, uint32_t v);
+void PutVarint64(std::string *dst, uint64_t v);
+//变长整数编码
+bool GetVarint32(Slice *input, uint32_t *value);
+bool GetVarint64(Slice *input, uint64_t *value);
+
+int VarintLength(uint64_t v);
+const char *GetVarint32Ptr(const char *p, const char *limit, uint32_t *value);
+const char *GetVarint64Ptr(const char *p, const char *limit, uint64_t *v);
+//先写长度（varint）再写数据
+bool GetLengthPrefixedSlice(Slice *input, Slice *result);
+void PutLengthPrefixedSlice(std::string *dst, const Slice &value);
+
+}  
