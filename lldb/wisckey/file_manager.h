a9eb0f6a93df15ab8450afae49de45554762a254
lfd <2281409362@qq.com>
2026-04-29 19:26:39 +0800

    wisckey

---
+++ lldb/wisckey/file_manager.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include <db/env.h>
+
+#include "vtable_format.h"
+
+namespace lldb {
+
+class DB;
+
+namespace wisckey {
+
+class VFileHandle {
+ public:
+  VFileHandle(uint64_t number, const std::string &name,
+              std::unique_ptr<WriteableFileWriter> file)
+      : number_(number), name_(name), file_(std::move(file)) {}
+
+  uint64_t GetNumber() const { return number_; }
+
+  const std::string &GetName() const { return name_; }
+
+  WriteableFileWriter *GetFile() const { return file_.get(); }
+
+ private:
+  friend class VFileManager;
+
+  uint64_t number_;
+  std::string name_;
+  std::unique_ptr<WriteableFileWriter> file_;
+};
+
+class VFileManager {
+ public:
+  VFileManager(DB *db) : db_(db) {}
+//创建一个新的 VTable 文件，并返回句柄
+  Status NewFile(std::unique_ptr<VFileHandle> *handle);
+
+  Status FinishFile(std::shared_ptr<VFileMeta> file,
+                    std::unique_ptr<VFileHandle> &&handle);
+
+  Status DeleteFile(std::unique_ptr<VFileHandle> &&handle);
+
+  Status BatchFinishFiles(
+      const std::vector<std::pair<std::shared_ptr<VFileMeta>,
+                                  std::unique_ptr<VFileHandle>>> &files);
+
+  Status BatchDeleteFiles(
+      const std::vector<std::unique_ptr<VFileHandle>> &handles);
+
+ private:
+  friend class FileHandle;
+
+  DB *db_;
+};
+}  // namespace wisckey
+}  
---
+++ lldb/wisckey/vfile_builder.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "db/env.h"
+#include "table/options.h"
+
+#include "vtable_format.h"
+
+namespace lldb {
+namespace wisckey {
+
+class VFileBuilder {
+ public:
+  VFileBuilder(const Options &options, WriteableFileWriter *file)
+      : options_(options), file_(file), encoder_(options_.compression) {}
+
+  // Adds the record to the file and points the handle to it.
+  void Add(const VRecord &record, VHandle *handle);
+
+  // Returns non-ok iff some error has been detected.
+  Status status() const { return status_; }
+
+  // Finishes building the table.
+  // REQUIRES: Finish(), Close() have not been called.
+  Status Finish();
+
+  void Close();
+
+ private:
+  bool ok() const { return status().ok(); }
+
+  Options options_;
+  WriteableFileWriter *file_;
+
+  Status status_;
+  Encoder encoder_;
+};
+}  // namespace wisckey
+
+}  
